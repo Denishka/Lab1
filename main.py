f786f889a227362c932f7d620bdc3bb3f452cacd
denis <denis.sabirzianow@yandex.ru>
2022-02-15 22:40:14 +0400

    AffineTransform

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-# This is a sample Python script.
+import json
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from matplotlib import pyplot as plt
+from skimage import transform
+from skimage.exposure import histogram
+from skimage.io import imread, imshow
+
+# читаем из файла
+with open('settings.json') as json_file:
+    json_data = json.load(json_file)
+
+for entry in json_data.keys():
+    print(f'Название параметра: {entry}, значение параметра: {json_data[entry]}')
+
+path = json_data['source_image_path']
+img = imread(path)
+print('Image shape: ', img.shape)
+
+v_scale_xy = json_data['scale_XY']
+v_shift_xy = json_data['shift_XY']
+v_angle = json_data['angle']
+
+tform = transform.AffineTransform(scale=v_scale_xy, rotation=v_angle, translation=v_shift_xy)
+print('transform matrix: \n', tform.params)
+img_tf = transform.warp(img, tform)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
