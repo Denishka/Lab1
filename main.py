a0a2e8d4e90df75506e79aeb7f494d474a3346ba
denis <denis.sabirzianow@yandex.ru>
2022-02-15 22:48:46 +0400

    added function to create histograms

--- main.py
+++ main.py
@@ -24,29 +24,25 @@ tform = transform.AffineTransform(scale=v_scale_xy, rotation=v_angle, translatio
 print('transform matrix: \n', tform.params)
 img_tf = transform.warp(img, tform)
 
+
+def create_histogram_plot(img_par):
+    hist_red, bins_red = histogram(img[500:600, 300:400, 2])
+    hist_green, bins_green = histogram(img[500:600, 300:400, 1])
+    hist_blue, bins_blue = histogram(img[500:600, 300:400, 0])
+
+    plt.plot(bins_green, hist_green, color='green', linestyle='-', linewidth=1)
+    plt.plot(bins_red, hist_red, color='red', linestyle='-', linewidth=1)
+    plt.plot(bins_blue, hist_blue, color='blue', linestyle='-', linewidth=1)
+
+
 fig = plt.figure(figsize=(7, 7))
 fig.add_subplot(2, 2, 1)
 imshow(img)
 fig.add_subplot(2, 2, 2)
 imshow(img_tf)
-
-hist_red, bins_red = histogram(img[500:600, 300:400, 2])
-hist_green, bins_green = histogram(img[500:600, 300:400, 1])
-hist_blue, bins_blue = histogram(img[500:600, 300:400, 0])
-
 fig.add_subplot(2, 2, 3)
-plt.plot(bins_green, hist_green, color='green', linestyle='-', linewidth=1)
-plt.plot(bins_red, hist_red, color='red', linestyle='-', linewidth=1)
-plt.plot(bins_blue, hist_blue, color='blue', linestyle='-', linewidth=1)
-
-hist_red, bins_red = histogram(img_tf[500:600, 300:400, 2])
-hist_green, bins_green = histogram(img_tf[500:600, 300:400, 1])
-hist_blue, bins_blue = histogram(img_tf[500:600, 300:400, 0])
-
+create_histogram_plot(img)
 fig.add_subplot(2, 2, 4)
-plt.plot(bins_green, hist_green, color='green', linestyle='-', linewidth=1)
-plt.plot(bins_red, hist_red, color='red', linestyle='-', linewidth=1)
-plt.plot(bins_blue, hist_blue, color='blue', linestyle='-', linewidth=1)
-#show()
+create_histogram_plot(img_tf)
 
 fig.savefig(json_data['image_save_path'])
